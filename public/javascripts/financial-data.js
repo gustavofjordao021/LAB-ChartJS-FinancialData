4b5f3e2bbc16acd43ab8c5885086c7afa0709459
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-02-12 12:10:25 -0500

    Clean front-end script

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,33 +1,17 @@
-window.onload = function() {
-
-    const express = require('express');
-    const axios = require('axios');
-    const Chart = require('chart.js');
-    const router = express.Router();
-
-    // Setting up Axios' dependencies
-    const ctx = document.getElementById("myChart").getContext("2d");
-    const apiUrl = "https://api.coindesk.com/v1/bpi/historical/close.json"
-
-    /* GET home page. */
-    router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-    });
-
-    // GET Financial data
-    axios
-    .get(apiUrl)
+axios
+    .get("https://api.coindesk.com/v1/bpi/historical/close.json")
     .then(responseFromAPI => {
         console.log(responseFromAPI);
         printChart(responseFromAPI.data);
     })
     .catch(err => console.log("Error while getting the data: ", err));
 
-    let printChart = bpiData => {
-        const chartData = bpiData["Time series - 30 rolling days"];
-        // const bpiDates = Object.keys(chartData);
-        // const bpiPrice = stockDates.map(date => chartData[date]["4. close"])
-        const myChart = new Chart(ctx, {
+let printChart = bpiData => {
+    const chartData = bpiData["Time series - 30 rolling days"];
+    // const bpiDates = Object.keys(chartData);
+    // const bpiPrice = stockDates.map(date => chartData[date]["4. close"])
+    const ctx = window.document.getElementById("myChart").getContext("2d");
+    const myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: bpiDates,
@@ -38,8 +22,5 @@ window.onload = function() {
             data: bpiPrice
             }]
         }
-        });
-    }
-
-  module.exports = router;
+    });
 }
