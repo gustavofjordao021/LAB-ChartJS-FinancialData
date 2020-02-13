d7e0a8683d116f56bf3bfda6ca39848dc505a3b1
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-02-12 22:10:36 -0500

    Clean code

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,41 +1,36 @@
 // Axios config
-window.onload = fetchData = () => {
-    axios.get("https://api.coindesk.com/v1/bpi/historical/close.json")
-        .then(responseFromAPI => printChart(responseFromAPI.data.bpi))
-        .catch(err => console.log("Error while getting the data: ", err));
-}
+axios.get("https://api.coindesk.com/v1/bpi/historical/close.json")
+    .then(responseFromAPI => printChart(responseFromAPI.data.bpi))
+    .catch(err => console.log("Error while getting the data: ", err));
 
 const ctx = document.getElementById('myChart').getContext('2d');
 
 // Input filtering
-let inputArray = [];
-let dateStart = document.getElementById("dateInput1").value; 
-let dateEnd = document.getElementById("dateInput2").value; 
-let currencyInput = document.getElementById("currency").value;
+let dateStart = ""; 
+let dateEnd = "";
+let currencyInput = "";
 
-inputArray.push(dateStart, dateEnd, currencyInput);
+document.getElementById("submit").onclick = function() {
+    dateStart = document.getElementById("dateInput1").value; 
+    dateEnd = document.getElementById("dateInput2").value;
+    currencyInput = document.getElementById("currency").value;
+    callFilterData(dateStart, dateEnd, currencyInput);
+};
 
 function callFilterData(dateStart, dateEnd, currency) {
     axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${dateStart}&end=${dateEnd}&currency=${currency}`)
-    .then(responseFromAPI => printChart(responseFromAPI.data.bpi))
+    .then(responseFromAPI => printChart(responseFromAPI.data.bpi, currency))
     .catch(err => console.log("Error while getting the data: ", err));
 }
-
-if (inputArray.length = 3) {
-    console.log(inputArray);
-    callFilterData(dateStart, dateEnd, currencyInput);
-} else if (inputArray.length > 3) {
-    inputArray = [];
-}
-
+    
 // Print Chart function
-const printChart = (data) => {
+const printChart = (data, currency) => {
     const financialChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: Object.keys(data),
             datasets: [{
-                label: "Bitcoin price in USD",
+                label: `Bitcoin price in ${currency || "USD"}`,
                 data: Object.values(data),
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
